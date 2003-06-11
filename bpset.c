ce9c5eeebcaa790f0902175a11539750bbd5042c
wojtekka <wojtekka@af26cbef-1f1d-416d-bc9f-7ed0cf577233>
2003-06-11 22:56:16 +0000

    bindprivs-0.6

--- bpset.c
+++ bpset.c
@@ -1,6 +1,6 @@
 /*
  * bindprivs access list manager
- * (c) 1998-2002 wojtek kaniewski <wojtekka@dev.null.pl>
+ * (c) 1999-2002 wojtek kaniewski <wojtekka@dev.null.pl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
